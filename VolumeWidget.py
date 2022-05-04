59b236e605608aa244c3485bd4c942fab9df2cd3
Savana Hughes <savanahughes@gmail.com>
2022-05-04 15:13:56 -0400

    added Volume label and placed it in the right place in the grid of the Volume Widget

--- VolumeWidget.py
+++ VolumeWidget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QPushButton, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QDial
+from PyQt5.QtWidgets import QPushButton, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QDial, QLabel
 
 class VolumeWidget(QWidget):
     def __init__(self):
@@ -6,16 +6,39 @@ class VolumeWidget(QWidget):
         super().__init__()
         self.dial = QDial()
         self.dial.setMinimum(0)
-        self.dial.setMaximum(100)
-        self.dial.setValue(40)
-        # Generate Layout for Widget
+        self.dial.setMaximum(100) 
+        self.dial.setValue(40) #starting volume level
+        self.dial.setNotchesVisible(True)
+        #Add Volume Label
+        self.label = QLabel(self)
+        self.label.setText(str(self.dial.value()) + "            ")
+
+        #Change Volume Label as Dial is turned
+        self.dial.valueChanged.connect(self.get_volume_level)
+        
+
+        # Generate Layout for the volume Knob
         self.hbox = QHBoxLayout() 
         self.hbox.addStretch(1) # right aligns the widget
         self.hbox.addWidget(self.dial)
-        # self.vbox = QVBoxLayout()
-        # self.vbox.addStretch(-1)
-        # self.vbox.addLayout(self.hbox)
 
-        self.setLayout(self.hbox)
+        # Generate Layout for the volume label
+        self.hbox2 = QHBoxLayout()
+        self.hbox2.addStretch(1)
+        self.hbox2.addWidget(self.label)
+
+        # Add both horizontal layouts to a vertical layout so that volume knob is above label
+        self.vbox = QVBoxLayout()
+        self.vbox.addLayout(self.hbox)
+        self.vbox.addLayout(self.hbox2)
+        # self.vbox.addStretch(1) #this would bottom align the widget
+
+        #Set final layout
+        self.setLayout(self.vbox) 
+
+    def get_volume_level(self):
+        value = self.dial.value()
+        # setting text to the label
+        self.label.setText(str(value) + "            ")
 
 
