8b079888c96f0c5944695a2549e7d9b0d50f730a
MorisGoldshtein <morisgoldshtein@Moriss-MBP.fios-router.home>
2022-05-05 13:25:21 -0400

    Media Slider and Open Directory Visuals

--- MainWindow.py
+++ MainWindow.py
@@ -1,9 +1,9 @@
 
-from VolumeWidget import VolumeWidget
+from functools import partial
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QGridLayout, QDial
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QGridLayout, QMenuBar, QMenu, QPlainTextEdit, QFileDialog
 from MediaWidget import MediaWidget
-# from VolumeWidget import VolumeWidget
+from VolumeWidget import VolumeWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,6 +12,10 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Music Player")
         # self.setFixedSize(1200, 700)
         self.setGeometry(300, 150, 350, 300)
+
+        # MAKING THE WIDGETS self. ALLOWS OTHER OBJS TO ACCESS THESE THROUGH MainWindow, addWidget(Widget()) prevents this
+        self.mediaWidget = MediaWidget()
+        self.volumeWidget = VolumeWidget()
 
         self.mainWidget = QWidget()
         self.mainLayout = QGridLayout()
@@ -23,8 +27,24 @@ class MainWindow(QMainWindow):
         self.mainLayout.addWidget(QPushButton("Directory Place Holder"), 0,0) #the numbers are coordiantes that correspond to a location in the grid. (row, col)
         self.mainLayout.addWidget(QPushButton("Playlist Place Holder"), 2,0)
         self.mainLayout.addWidget(QPushButton("Place Holder"), 0,1)
-        self.mainLayout.addWidget(VolumeWidget(), 0,3)
-        self.mainLayout.addWidget(MediaWidget(),2,1, 2,3)
+                
+        self.mainLayout.addWidget(self.volumeWidget, 0, 3)
+        self.mainLayout.addWidget(self.mediaWidget,2 ,1, 2, 3)
+
+        menuBar = self.menuBar()
+        menu = menuBar.addMenu("File")
+        openFolderAction = menu.addAction("Open Folder")
+        openFolderAction.triggered.connect(partial(self.openFolder,self))
+        self.menuBar = menuBar
 
         self.mainWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self.mainWidget)
+
+    # The action.connect automatically sends a MainWindow as an arg
+    def openFolder(window, self): 
+        # Sourced from https://stackoverflow.com/questions/4286036/how-to-have-a-directory-dialog
+        folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        print(folder)
+
+
+
--- problemDocs.md
+++ problemDocs.md
@@ -5,3 +5,7 @@ Details of problems encountered throughout the project and how they were solved
 # Problem 1 - Savana Hughes - Ocurred 05/04/22
     Description: Layout of components were conflicting with aspects each contributor had created. 
     Solution: Created a Grid Layout system for our gui, so that each component can be added to a specifc row/col of the main window. Each component can have its own defined layout, as long as it is inserted in the main window grid at the appropriate location. 
+
+# Problem 2 - Moris Goldshtein - Ocurred 05/04/22
+    Description: In GridLayout, spanning 3 columns for the music track slider created a limitation for increasing the width of the slider. If a user friendly size was used, the MainWindow changed size along with the widths of the buttons above the slider. 
+    Solution: Spanning 5 columns starting at col 0 gives far more leeway for increasing the width of the slider as we now cannot go beyond the width of the layout without causing the same problem, which would not be intended anyway. The only caveat is to make sure that a selected width prevents the slider from touching the labels next to it, which has been done. 
